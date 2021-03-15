c250aae27c9ac884a532af798fa6f57e71aebfe6
LAPTOP-KD61MG7G\Duc <lehuuduc271295@gmail.com>
2021-03-15 21:54:40 +0700

    fix lai ejs

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const db=require('./database');
 
 
 
-
+app.use(expresslayouts);
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'public')));
