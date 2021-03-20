4868908560102b40160bea13557ba35786ab2671
Thuan Han <thuanhan@gmail.com>
2021-03-20 13:32:58 +0700

    xoa node_modules

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,24 +1,16 @@
-
-
-
-class NewsController{
-
-    // GET  /news
-    new(req,res){
-        res.render('news',{title:'new page',layout:'./layouts/main'});
-
-    }
-    show(req,res){
-        res.send('new detail');
-
-    }
-    index(req,res){
-        res.render('home',{title:'Home page',layout:'./layouts/main'});
+class NewsController {
+  // GET  /news
+  new(req, res) {
+    res.render("news", { title: "new page", layout: "./layouts/main" });
+  }
+  show(req, res) {
+    res.send("new detail");
+  }
+  index(req, res) {
+    res.render("home", { title: "Home page", layout: "./layouts/main" });
+  }
+  search(req, res) {
+    res.render("search", { title: "search page", layout: "./layouts/main" });
+  }
 }
-search(req,res){
-    res.render('search',{title:'search page',layout:'./layouts/main'});
-}
-
-    
-}   
-module.exports = new NewsController;
+module.exports = new NewsController();
