4a565cfd93a5c74ea625b4e237d6c3d04eb59706
Thuan Han <thuanhan@gmail.com>
2021-03-21 09:10:54 +0700

    Format some files

--- src/app/controllers/DiemController.js
+++ src/app/controllers/DiemController.js
@@ -1,39 +1,39 @@
 const myModel = require("../../models/MyModel");
 class DiemController {
-  getCreate(req, res) {
-    var userID = req.session.userID;
-    var user = req.session.user;
-    if (userID !== null) {
-      res.redirect("/login");
-    } else res.render("./diem/create", { title: "create", layout: "./layouts/main" });
-  }
+    getCreate(req, res) {
+        var userID = req.session.userID;
+        var user = req.session.user;
+        if (userID !== null) {
+            res.redirect("/login");
+        } else res.render("./diem/create", { title: "create", layout: "./layouts/main" });
+    }
 
-  postCreate(req, res) {
-    var mssv = req.body.mssv;
-    var mmh = req.body.mmh;
-    var dqt = req.body.dqt;
-    var dt = req.body.dt;
+    postCreate(req, res) {
+        var mssv = req.body.mssv;
+        var mmh = req.body.mmh;
+        var dqt = req.body.dqt;
+        var dt = req.body.dt;
 
-    myModel.inputDiem(mssv, mmh, dqt, dt, (data) => {
-      console.log(data);
-    });
-    res.render("./diem/create", { title: "create", layout: "./layouts/main" });
-  }
+        myModel.inputDiem(mssv, mmh, dqt, dt, (data) => {
+            console.log(data);
+        });
+        res.render("./diem/create", { title: "create", layout: "./layouts/main" });
+    }
 
-  fetchDiem(req, res) {
-    myModel.outputDiem((data) => {
-      res.render("./diem/showdiem", { title: "show", layout: "./layouts/main", user: data });
-    });
-  }
-  getSearch(req, res) {
-    res.render("./diem/searchdiem", { title: "search", layout: "./layouts/main" });
-  }
-  postSearch(req, res) {
-    var mssv = req.body.timkiem;
-    myModel.searchDiem(mssv, (data) => {
-      res.render("./diem/searchdiempost", { layout: "./layouts/site", user: data });
-    });
-  }
+    fetchDiem(req, res) {
+        myModel.outputDiem((data) => {
+            res.render("./diem/showdiem", { title: "show", layout: "./layouts/main", user: data });
+        });
+    }
+    getSearch(req, res) {
+        res.render("./diem/searchdiem", { title: "search", layout: "./layouts/main" });
+    }
+    postSearch(req, res) {
+        var mssv = req.body.timkiem;
+        myModel.searchDiem(mssv, (data) => {
+            res.render("./diem/searchdiempost", { layout: "./layouts/site", user: data });
+        });
+    }
 }
 
 module.exports = new DiemController();
--- src/models/MyModel.js
+++ src/models/MyModel.js
@@ -1,22 +1,22 @@
-const db = require('../database');
+const db = require("../database");
 
 class GetQuery {
     inputDiem(mssv, mmh, dtb, dt, callback) {
-        var sql = 'insert into diem (Ma_SV,Ma_MH,DQT,DT) values(?,?,?,?)';
+        var sql = "insert into diem (Ma_SV,Ma_MH,DQT,DT) values(?,?,?,?)";
         db.query(sql, [mssv, mmh, dtb, dt], (err, data) => {
             if (err) throw err;
             return callback(data);
         });
     }
     outputDiem(cb) {
-        var sql = 'select * from diem';
+        var sql = "select * from diem";
         db.query(sql, (err, data, field) => {
             if (err) throw err;
             return cb(data);
         });
     }
     searchDiem(tim, cb) {
-        var sql = 'select * from diem where Ma_SV=?';
+        var sql = "select * from diem where Ma_SV=?";
         db.query(sql, [tim], (err, data) => {
             if (err) throw err;
             return cb(data);
