3c567f49d724577694216120281d7eb727fe7773
Thuan Han <thuanhan@gmail.com>
2021-03-21 09:07:28 +0700

    Remove node_modules

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,13 @@
+const newsRouter = require("./news");
+const diemRouter = require("./diem");
+const authenticationRouter = require("./authentication");
 
-const newsRouter = require('./news');
-const diemRouter=require('./diem');
-const authenticationRouter=require('./authentication');
-
-function  route(app){
-
+function route(app) {
     // app.get('/new',(req,res)=>{
-     //   res.render('news');
+    //   res.render('news');
     //});
-    app.use('/diem',diemRouter);
-    app.use('/',authenticationRouter);
-    app.use('/',newsRouter);
-    
-
-    
-
-    
+    app.use("/diem", diemRouter);
+    app.use("/", authenticationRouter);
+    app.use("/", newsRouter);
 }
-module.exports =route;
+module.exports = route;
